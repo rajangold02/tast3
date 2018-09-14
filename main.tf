88a669f2c02ecb347eb0408642212ae603915436
rajangold02 <rajangold02@gmail.com>
2018-09-14 17:55:41 -0400

    Final Changes

--- main.tf
+++ main.tf
@@ -117,14 +117,14 @@ resource "aws_security_group" "instance" {
     from_port = 22
     to_port   = 22
 
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["210.18.176.193/32"]
   }
 
   ingress {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = ["${aws_security_group.elb.id}"]
   }
 
   egress {
